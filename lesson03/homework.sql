8ff65780c31c77b05d00a7f889bc9b8cfc5e6309
Will Parry <lordgeralt@mail.ru>
2020-05-13 01:05:48 +0300

    Lesson 03 is ready to be inspected

--- lesson03/homework.sql
+++ lesson03/homework.sql
@@ -1,45 +1,68 @@
 # Lesson 03 Homework: Data Definition Language
-# Microblog, 
-
 USE `vk`;
 
-# Это та часть, которая раньще называлась Стена в ВК (а сейчас микроблог), куда пользователь может добавлять записи
-DROP TABLES IF EXISTS `microblog`;
-CREATE TABLE `microblog` (
+# Всё-таки я переделаю таблицу media, как домашнее задание
+
+# MEDIAFILES_TYPES
+DROP TABLE IF EXISTS `mediafiles_types`;
+CREATE TABLE `mediafiles_types` (
 	`id` SERIAL,
-    `user_id` BIGINT UNSIGNED NOT NULL,
-    `header` TEXT NULL,
-    `attach1` BIGINT UNSIGNED NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    
+    PRIMARY KEY (`id`)
+);
+# MEDIAFILES
+DROP TABLE IF EXISTS `mediafiles`;
+CREATE TABLE `mediafiles`(
+	`id` SERIAL,
+    `mediafile_type_id` BIGINT UNSIGNED NOT NULL,
+    `filename` VARCHAR(255) NOT NULL,
+    `size` INT NOT NULL,
+    `metadata` JSON NOT NULL,
     
     PRIMARY KEY (`id`),
-    
-	FOREIGN KEY (``) REFERENCES ``(``)
-		ON UPDATE NO ACTION
-		ON DELETE NO ACTION
+    FOREIGN KEY (`mediafile_type_id`) REFERENCES `mediafiles_types`(`id`)
 );
 
-DROP TABLES IF EXISTS ``;
-CREATE TABLE `` (
-	`` SERIAL,
+# ALBUMS_TYPES
+DROP TABLE IF EXISTS `albums_types`;
+CREATE TABLE `albums_types` (
+	`id` SERIAL,
+    `name` VARCHAR(255) NOT NULL,
     
-    PRIMARY KEY (``),
-    
-    INDEX `` (``),
-    
-	FOREIGN KEY (``) REFERENCES ``(``)
-		ON UPDATE NO ACTION
-		ON DELETE NO ACTION
+    PRIMARY KEY (`id`)
 );
-
-DROP TABLES IF EXISTS ``;
-CREATE TABLE `` (
-	`` SERIAL,
+# MEDIA_ALBUMS
+DROP TABLE IF EXISTS `media_albums`;
+CREATE TABLE `media_albums` (
+	`id` SERIAL,
+    `name` VARCHAR(255),
+    `user_id` BIGINT UNSIGNED NOT NULL,
+    `album_type_id` BIGINT UNSIGNED NOT NULL,
     
-    PRIMARY KEY (``),
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
+    FOREIGN KEY (`album_type_id`) REFERENCES `albums_types`(`id`)
+);
+# MEDIAFILE_HOLDER
+DROP TABLE IF EXISTS `mediafile_holder`;
+CREATE TABLE `mediafile_holder` (
+	`mediafile_id` BIGINT UNSIGNED NOT NULL,
+    `holder_id` BIGINT UNSIGNED NOT NULL,
+	`album_id` BIGINT UNSIGNED NULL,
     
-    INDEX `` (``),
+    PRIMARY KEY(`mediafile_id`, `holder_id`),
+    FOREIGN KEY (`mediafile_id`) REFERENCES `mediafiles`(`id`),
+    FOREIGN KEY (`holder_id`) REFERENCES `users`(`id`),
+    FOREIGN KEY (`album_id`) REFERENCES `media_albums`(`id`)
+);
+# MEDIAFILE_LIKER
+DROP TABLE IF EXISTS `mediafile_liker`;
+CREATE TABLE `mediafile_liker` (
+	`mediafile_id` BIGINT UNSIGNED NOT NULL,
+    `liker_id` BIGINT UNSIGNED NOT NULL,
     
-	FOREIGN KEY (``) REFERENCES ``(``)
-		ON UPDATE NO ACTION
-		ON DELETE NO ACTION
+    PRIMARY KEY (`mediafile_id`, `liker_id`),
+    FOREIGN KEY (`mediafile_id`) REFERENCES `mediafiles`(`id`),
+    FOREIGN KEY (`liker_id`) REFERENCES `users`(`id`)
 );
