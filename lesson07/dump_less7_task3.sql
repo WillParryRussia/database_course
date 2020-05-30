3adc67652bd3cbb34afc3b0f0fc02ad7c61533bc
Will Parry <lordgeralt@mail.ru>
2020-05-30 09:42:44 +0300

    Lesson 07 is ready to be checked

---
+++ lesson07/dump_less7_task3.sql
@@ -0,0 +1,77 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Linux (x86_64)
+--
+-- Host: localhost    Database: task3
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cities`
+--
+
+DROP TABLE IF EXISTS `cities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cities` (
+  `label` varchar(128) COLLATE utf8_bin NOT NULL,
+  `name` varchar(128) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`label`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cities`
+--
+
+LOCK TABLES `cities` WRITE;
+/*!40000 ALTER TABLE `cities` DISABLE KEYS */;
+INSERT INTO `cities` VALUES ('irkutsk','Иркутск'),('kazan','Казань'),('moscow','Москва'),('novgorod','Новгород'),('omsk','Омск');
+/*!40000 ALTER TABLE `cities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `flights`
+--
+
+DROP TABLE IF EXISTS `flights`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `flights` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `from` varchar(128) COLLATE utf8_bin NOT NULL,
+  `to` varchar(128) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `flights`
+--
+
+LOCK TABLES `flights` WRITE;
+/*!40000 ALTER TABLE `flights` DISABLE KEYS */;
+INSERT INTO `flights` VALUES (1,'moscow','omsk'),(2,'novgorod','kazan'),(3,'irkutsk','moscow'),(4,'omsk','irkutsk'),(5,'moscow','kazan');
+/*!40000 ALTER TABLE `flights` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-05-30  9:32:42
--- lesson07/script.sql
+++ lesson07/script.sql
@@ -14,18 +14,26 @@ SELECT DISTINCT
 # Вложенный запрос с ключевым словом EXISTS
 SELECT * FROM `users`
 	WHERE EXISTS (SELECT * FROM `orders` WHERE `client_id` = `users`.`id`);
-# Запрос через объединение таблиц
-
 ################################################################################
 -- Task #2 : Выведите список товаров products и разделов catalogs, 
 -- который соответствует товару.
 -- Дамп базы данных для этого таска в файле lesson07/dump_less7_task2.sql
-# your script is here
-
+# Запрос с вложенным SELECT 
+SELECT `name`,
+	(SELECT `name` FROM `catalogs` WHERE `catalogs`.`id` = `catalog_id`) AS `Catalog`
+    FROM `products`;
+# Запрос через объединение таблиц JOIN
+SELECT `products`.`name`, `catalogs`.`name` AS `Catalog`
+	FROM `products`
+    RIGHT JOIN `catalogs`
+    ON `catalogs`.`id` = `catalog_id`;
 ################################################################################
--- Task #3 : по желанию) Пусть имеется таблица рейсов flights (id, from, to) 
+-- Task #3 : (по желанию) Пусть имеется таблица рейсов flights (id, from, to) 
 -- и таблица городов cities (label, name). Поля from, to и label содержат 
 -- английские названия городов, поле name — русское. 
 -- Выведите список рейсов flights с русскими названиями городов.
 -- Дамп базы данных для этого таска в файле lesson07/dump_less7_task3.sql
-# your script is here
+SELECT `id` AS `Номер рейса`,
+	(SELECT `name` FROM `cities` WHERE `flights`.`from` = `label`) AS `Откуда`,
+    (SELECT `name` FROM `cities` WHERE `flights`.`to` = `label`) AS `Куда`
+	FROM `flights`;
