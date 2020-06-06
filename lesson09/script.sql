ed705ea78249712c0a46c36287fd6416c495e904
Will Parry <lordgeralt@mail.ru>
2020-06-06 10:11:34 +0300

    functions are ready

--- lesson09/script.sql
+++ lesson09/script.sql
@@ -42,12 +42,45 @@ COMMIT;
 -- 3.1. Создайте хранимую функцию hello(), которая будет возвращать приветствие, в зависимости от текущего времени суток. 
 -- С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро", с 12:00 до 18:00 функция должна возвращать фразу "Добрый день", 
 -- с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".
-
+DELIMITER |
+CREATE FUNCTION `hello` ()
+RETURNS TEXT DETERMINISTIC
+BEGIN
+	DECLARE ch INT(2);
+	SET ch = HOUR(CURRENT_TIME());
+	RETURN IF(ch >= 0 AND ch < 6, 'Good nignt', IF(ch >= 6 AND ch < 12, 'Good morning', IF(ch >= 12 AND ch < 18, 'Good day', 'Good evening')));
+END;
+| DELIMITER ;
+SELECT hello();
 
 -- 3.2. В таблице products есть два текстовых поля: name с названием товара и description с его описанием. 
 -- Допустимо присутствие обоих полей или одно из них. Ситуация, когда оба поля принимают неопределенное значение NULL неприемлема. 
 -- Используя триггеры, добейтесь того, чтобы одно из этих полей или оба поля были заполнены. 
 -- При попытке присвоить полям NULL-значение необходимо отменить операцию.
+USE `test`;
+DELIMITER |
+CREATE TRIGGER `insert2products`
+	BEFORE INSERT ON `products`
+	FOR EACH ROW
+	BEGIN
+		IF NEW.`name` IS NOT NULL AND NEW.`description` IS NOT NULL
+		THEN
+			INSERT INTO `products` (`name`,`description`) VALUES (NEW.`name`, NEW.`description`);
+		END IF;
+	END;
+| DELIMITER ;
+
+
+SELECT HOUR(CURTIME());
+CREATE DATABASE `test`;
+USE `test`;
+CREATE TABLE `products` (`id` SERIAL PRIMARY KEY, `name` VARCHAR(32), `description` VARCHAR(32));
+USE `test`;
+INSERT INTO `products` (`name`,`description`) VALUES ('Item 1', 'Desc of I1');
+INSERT INTO `products` (`name`) VALUES ('Item 2');
+INSERT INTO `products` (`description`) VALUES ('Desc of I3');
+INSERT INTO `products` () VALUES ();
+SELECT * FROM `products`;
 
 
 -- 3.3. (по желанию) Напишите хранимую функцию для вычисления произвольного числа Фибоначчи. Числами Фибоначчи называется последовательность 
